7dfbb70f6da5e35764e6212a36c3dff393859496
Noah Gambee <noah@gambee.dev>
2020-02-27 09:05:34 -0700

    Add redux imports and dependencies to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
