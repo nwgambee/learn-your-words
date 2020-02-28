aa7ca5798fe75efd4bf0927708fba7a5702388c2
Noah Gambee <noah@gambee.dev>
2020-02-28 12:39:18 -0700

    Create boilerplate Header component and render through App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import SearchForm  from '../../containers/SearchForm/SearchForm.js'
 import CardContainer from '../../containers/CardContainer/CardContainer'
+import Header from '../Header/Header'
 
 const App = () => {
     return(
       <main className='main'>
+        <Header />
         <SearchForm />
         <CardContainer />
       </main>
--- src/containers/VocabCard/VocabCard.js
+++ src/containers/VocabCard/VocabCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 const VocabCard = ({ wordDetails }) => {
     console.log(wordDetails)
