2609785e596828c63f0086c6f0497a2bed89149c
Noah Gambee <noah@gambee.dev>
2020-02-28 12:53:41 -0700

    Refactor VocabCard into class component

--- src/containers/SearchForm/SearchForm.js
+++ src/containers/SearchForm/SearchForm.js
@@ -17,6 +17,7 @@ class SearchForm extends Component {
          e.preventDefault();
          searchVocabWord(this.state.query)
             .then(data => this.props.getVocabWord(data)) 
+        this.setState({query:''});
      }
      render() {
         const { query } = this.state
--- src/containers/VocabCard/VocabCard.js
+++ src/containers/VocabCard/VocabCard.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { mapStateToProps } from '../CardContainer/CardContainer';
 
-const VocabCard = ({ wordDetails }) => {
-    console.log(wordDetails)
-    // console.log(wordDetails.wordDetails.searchedWordDetails);
-
-    if (wordDetails) {
-        const { word } = wordDetails
-        const definition = wordDetails.results[0].definition
-        const partOfSpeech = wordDetails.results[0].partOfSpeech
-        return (
-         <section className='vocab-card' id={`${word}-card`}>
-             <button className='add-to-list-btn'>+</button>
-            <h1>{word}</h1>
-            <h2> {definition} </h2>
-            <h2> {partOfSpeech} </h2>
-         </section>
-        )
-    } else {
-        return(
-            null
-        )
+class VocabCard extends Component {
+    constructor() {
+        super();
+     }
+     addToList = () => {
+        console.log(this.props.wordDetails)
     }
+     render() {
+        if (this.props.wordDetails) {
+            const { word } = this.props.wordDetails
+            const definition = this.props.wordDetails.results[0].definition
+            const partOfSpeech = this.props.wordDetails.results[0].partOfSpeech
+            return (
+             <section className='vocab-card' id={`${word}-card`}>
+                 <button className='add-to-list-btn' onClick={() => this.addToList()}>+</button>
+                <h1>{word}</h1>
+                <h2> {definition} </h2>
+                <h2> {partOfSpeech} </h2>
+             </section>
+            )
+        } else {
+            return(
+                null
+            )
+        }
+     }
+
 }
 
-export default VocabCard;
+const mapDispatchToProps = dispatch => ({
+    
+})
+
+export default connect(null, mapDispatchToProps)(VocabCard);
