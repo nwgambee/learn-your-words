229f4e8d7cca955a4ecbe4aad3549e7737e71aca
Noah Gambee <noah@gambee.dev>
2020-02-28 19:58:01 -0700

    Add Routing for vocab list component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import SearchForm  from '../../containers/SearchForm/SearchForm.js'
 import CardContainer from '../../containers/CardContainer/CardContainer'
 import Header from '../Header/Header'
+import VocabList from '../../containers/VocabList/VocabList'
 
 const App = () => {
     return(
       <main className='main'>
         <Header />
-        <SearchForm />
+        <Route
+              exact path='/'
+              render={() => <SearchForm />}
+            />
         <CardContainer />
+        <Route
+              path='/vocab-list'
+              render={() => <VocabList />}
+            />
       </main>
     )
   }
