64f7610e29fd8055735a0be7a66df2b5a12b712f
Noah Gambee <noah@gambee.dev>
2020-03-02 10:53:13 -0700

    Add disabledButton state property

--- src/containers/VocabCard/VocabCard.js
+++ src/containers/VocabCard/VocabCard.js
@@ -4,30 +4,31 @@ import { addToList } from '../../actions/index';
 import './VocabCard.scss';
 import PropTypes from 'prop-types';
 
-
 export class VocabCard extends Component {
     constructor() {
         super();
+        this.state = {disabledButton: false}
      }
      addToList = () => {
-         // this is not working properly, duplicates can still be added for some reason
          if (!this.props.vocabList.includes(this.props.wordDetails)) {
             this.props.addToVocabList(this.props.wordDetails)
+            this.setState({disabledButton: true})
          }
     }
+    enableButton = () => {
+        this.setState({disabledButton: false})
+    }
      render() {
-        if (this.props.wordDetails) {
+         if (this.props.wordDetails) {
             const { word } = this.props.wordDetails
             const definition = this.props.wordDetails.results[0].definition
             const partOfSpeech = this.props.wordDetails.results[0].partOfSpeech
-            // const example = this.props.wordDetails.results[0].examples[0] || null;
             return (
              <section className='vocab-card' id={`${word}-card`}>
                 <h1 className='word-h1' >{word}</h1>
                 <h2 className='definition'> {definition} </h2>
-                {/* <h3 className='example-sentence'>Example: "{example || null}"</h3> */}
                 <h2 className='part-of-speech'>{partOfSpeech} </h2>
-                 <button className='add-to-list-btn' onClick={() => this.addToList()}>Add To Vocab List</button>
+                 <button disabled={this.state.disabledButton} className='add-to-list-btn' onClick={() => this.addToList()}>Add To Vocab List</button>
              </section>
             )
         } else {
@@ -36,6 +37,7 @@ export class VocabCard extends Component {
             )
         }
      }
+     
 
 }
 
