d82c5cfd680a03abc5d587d6dd862a218c987194
Noah Gambee <noah@gambee.dev>
2020-03-01 22:06:44 -0700

    Update unit tests for SearchForm

--- src/containers/SearchForm/SearchForm.test.js
+++ src/containers/SearchForm/SearchForm.test.js
@@ -13,16 +13,26 @@ describe('SearchForm', () => {
     it('should call handleSubmit when the submit button is clicked', () => {
       wrapper = shallow(<SearchForm />);
       wrapper.instance().handleSubmit = jest.fn();
-      // wrapper.find('.search-word-form').simulate('submit');
       const form = wrapper.find('form').first();
       form.simulate('submit')
       expect(wrapper.instance().handleSubmit).toHaveBeenCalled();
     })
-    it.skip('should update state when handleError is called', () => {
-
+    it('should update state when handleError is called', () => {
+      const startState = {query: '', error: ''};
+      wrapper = shallow(<SearchForm />);
+      wrapper.setState(startState);
+      const expected = {query: '', error: 'Sorry, we were not able to find a definition for that word. Make sure your spelling is correct!' };
+      wrapper.instance().handleError();
+      expect(wrapper.state('error')).toEqual(expected.error);
     })
-    it.skip('should update state when handleChange is called', () => {
-      
+    it('should update state when handleChange is called', () => {
+      const startState = {query: '', error: ''};
+      wrapper = shallow(<SearchForm />);
+      wrapper.setState(startState);
+      const mockEvent = {target: {name: 'query', value: 'hat'}};
+      const expected = {query: 'hat', error: ''};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state('query')).toEqual(expected.query);
     })
   })
   describe('mapDispatchToProps', () => {
