537ebc1ee8385de77444fbe94d2b33b808708fd9
Noah Gambee <noah@gambee.dev>
2020-03-01 21:12:36 -0700

    Add MSTP and MDTP tests for VocabCard component

---
+++ src/containers/VocabCard/VocabCard.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { addToList } from '../../actions/index' 
+import { VocabCard, mapStateToProps, mapDispatchToProps } from './VocabCard';
+// import { shallow } from 'enzyme';
+
+describe('mapStateToProps', () => {
+  it('should return an object with the vocabList array', () => {
+    const mockState = {
+      searchedWord: {word: 'hat', definition: 'this is a definition', type: 'noun'},
+      loadingStatus: true,
+      vocabList: [{word: 'hat'}, {word: 'cat'}],
+      someOtherProp: false
+    }
+    const expected = {
+      vocabList: [{word: 'hat'}, {word: 'cat'}],
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('should call dispatch with a addToVocabList action when addToVocabList is called', () => {
+    const mockDispatch = jest.fn();
+    const action = addToList('word');
+    const mappedProps = mapDispatchToProps(mockDispatch)
+
+    mappedProps.addToVocabList('word');
+    expect(mockDispatch).toHaveBeenCalledWith(action);
+  })
+})
