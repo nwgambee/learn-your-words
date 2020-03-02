39832add3227df2af3d5faa6702588539a8be793
Noah Gambee <noah@gambee.dev>
2020-03-01 22:56:55 -0700

    Add shuffle method

--- src/containers/PracticeCards/PracticeCards.js
+++ src/containers/PracticeCards/PracticeCards.js
@@ -8,14 +8,41 @@ export class PracticeCards extends Component {
     super();
  }
 
+ shuffleList(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
  render() {
+   let shuffledList = this.shuffleList(this.props.vocabList);
+   let cards = shuffledList.map(word => {
+    return (
+           <section className='list-card' key={word.word}>
+             <h1 className='list-word-h1'>{word.word}</h1>
+             <h2 className='list-definition'>{word.results[0].definition}</h2>
+             <h2 className='list-part-of'>{word.results[0].partOfSpeech}</h2>
+             <button className='remove-from-list-btn' onClick={() => this.props.removeFromList(word.word)}>Remove From List</button>
+           </section> 
+
+  )
+ })
    return (
-     <section className='practice-container'>
-       <h1>Practice!</h1>
+     <section className='list-container'>
+       {cards}
      </section>
    )
  }
-}
+} 
 
 export const mapStateToProps = state => ({
   vocabList: state.vocabList
