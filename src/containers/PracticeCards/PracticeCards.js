9d8effd03e5fe8f293f3b92b0c83824f4ca3b84f
Noah Gambee <noah@gambee.dev>
2020-03-02 08:50:00 -0700

    Add checkGuess button to practice cards and begin functionality to check guess

--- src/containers/PracticeCards/PracticeCards.js
+++ src/containers/PracticeCards/PracticeCards.js
@@ -6,6 +6,11 @@ import PropTypes from 'prop-types';
 export class PracticeCards extends Component {
   constructor() {
     super();
+    this.state = {guess: ''}
+ }
+
+ handleChange = e => {
+   this.setState({ [e.target.name]: e.target.value })
  }
 
  shuffleList(array) {
@@ -22,23 +27,42 @@ export class PracticeCards extends Component {
   }
   return array;
 }
+checkGuess = (word, e) => {
+  if(this.state.guess.toLowerCase() === word.toLowerCase()) {
+    console.log('correct guess!')
+    // replace input with word and remove check guess button
+  } else {
+    console.log('incorrect guess');
+    // prompt 'incorrect guess' message
+  }
+}
 
  render() {
+  //  let shuffledList = [];
+  //  let cards;
    // render these cards with an input to replace .list-word-h1. Add a button to check answer. 
   //  When that is clicked, run helper function to check if the typed word matches the underlying word (maybe use id property of word.word and check against that).
   // if check is successful, uncover h1 to reveal correct answer. Prompt 'incorrect guess' message otherwise
-   let shuffledList = this.shuffleList(this.props.vocabList);
-   let cards = shuffledList.map(word => {
-    return (
-           <section className='list-card' key={word.word}>
-             <h1 className='list-word-h1'>{word.word}</h1>
-             <h2 className='list-definition'>{word.results[0].definition}</h2>
-             <h2 className='list-part-of'>{word.results[0].partOfSpeech}</h2>
-             <button className='remove-from-list-btn' onClick={() => this.props.removeFromList(word.word)}>Remove From List</button>
-           </section> 
+    let shuffledList = this.shuffleList(this.props.vocabList);
+    let cards = shuffledList.map(word => {
+     return (
+            <section className='list-card' key={word.word}>
+              <input className='list-word-input'
+               type='text'
+               placeholder='Word'
+               name={`guess`}
+               // value={`this.state.guess-${word.word}` || ''} 
+               onChange={this.handleChange}
+              />
+              <button className='check-guess' onClick={(e) => this.checkGuess(word.word, e)}>Check Guess</button>
+              <h2 className='list-definition'>{word.results[0].definition}</h2>
+              <h2 className='list-part-of'>{word.results[0].partOfSpeech}</h2>
+              {/* <button className='remove-from-list-btn' onClick={() => this.props.removeFromList(word.word)}>Remove From List</button> */}
+            </section> 
+ 
+   )
+  })
 
-  )
- })
    return (
      <section className='list-container'>
        {cards}
