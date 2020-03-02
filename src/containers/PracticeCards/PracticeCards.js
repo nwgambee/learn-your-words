c9396c09dceb378280457a7b2330ab394cd73b68
Noah Gambee <noah@gambee.dev>
2020-03-02 09:04:27 -0700

    Update console logs and pseudocode

--- src/containers/PracticeCards/PracticeCards.js
+++ src/containers/PracticeCards/PracticeCards.js
@@ -29,8 +29,6 @@ export class PracticeCards extends Component {
 }
 checkGuess = (word, e) => {
   if(this.state.guess.toLowerCase() === word.toLowerCase()) {
-    console.log('correct guess!')
-    // replace input with word and remove check guess button
     this.setState({ guess: '', correctlyGuessedWords: [...this.state.correctlyGuessedWords, word.toLowerCase()]})
 
   } else {
@@ -40,23 +38,15 @@ checkGuess = (word, e) => {
 }
 
  render() {
-  //  let shuffledList = [];
-  //  let cards;
-   // render these cards with an input to replace .list-word-h1. Add a button to check answer. 
-  //  When that is clicked, run helper function to check if the typed word matches the underlying word (maybe use id property of word.word and check against that).
-  // if check is successful, uncover h1 to reveal correct answer. Prompt 'incorrect guess' message otherwise
     let shuffledList = this.shuffleList(this.props.vocabList);
     let cards = shuffledList.map(word => {
       if(this.state.correctlyGuessedWords.includes(word.word.toLowerCase())) {
         return (
           <section className='list-card' key={word.word}>
             <h1 className='list-word-input'> {word.word}</h1>
-            {/* <button className='check-guess' onClick={(e) => this.checkGuess(word.word, e)}>Check Guess</button> */}
             <h2 className='list-definition'>{word.results[0].definition}</h2>
             <h2 className='list-part-of'>{word.results[0].partOfSpeech}</h2>
-            {/* <button className='remove-from-list-btn' onClick={() => this.props.removeFromList(word.word)}>Remove From List</button> */}
           </section> 
-
         )  
       } 
      return (
@@ -65,16 +55,14 @@ checkGuess = (word, e) => {
                type='text'
                placeholder='Word'
                name={`guess`}
-               // value={`this.state.guess-${word.word}` || ''} 
                onChange={this.handleChange}
               />
               <button className='check-guess' onClick={(e) => this.checkGuess(word.word, e)}>Check Guess</button>
               <h2 className='list-definition'>{word.results[0].definition}</h2>
               <h2 className='list-part-of'>{word.results[0].partOfSpeech}</h2>
-              {/* <button className='remove-from-list-btn' onClick={() => this.props.removeFromList(word.word)}>Remove From List</button> */}
             </section> 
  
-   )
+    )
   })
 
    return (
